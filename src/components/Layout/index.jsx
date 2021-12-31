6aa5ada9174ee31bfde73449e21fdf368e90ef52
winf42 <96917798+winf42@users.noreply.github.com>
2022-01-01 01:40:01 +0800

    Update index.jsx

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -23,7 +23,7 @@ const Layout = ({ children }) => {
       </Helmet>
       <Header title={siteTitle} />
       <div className="pa3 pa5-l">{children}</div>
-      <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=1328640893&auto=1&height=66"></iframe>
+ 
     </div>
   );
 };
