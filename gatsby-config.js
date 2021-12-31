da4bebac22ce14ccdf61532f5d69cd06e64afb69
winf42 <96917798+winf42@users.noreply.github.com>
2022-01-01 05:06:19 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     siteUrl: 'https://winf42.github.io/running',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
-    navLinks: [
+    //navLinks: [
       /*{
         name: 'Blog',
         url: 'https://winf42.github.io/running/404.html',
@@ -14,7 +14,7 @@ module.exports = {
         name: 'About',
         url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
       },*/
-    ],
+   // ],
   },
   plugins: [
     'gatsby-transformer-json',
