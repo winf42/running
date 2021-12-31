999bbf22a17e819687a05f58ba2f049a4f6091a2
winf42 <96917798+winf42@users.noreply.github.com>
2021-12-31 23:42:07 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/`, // Change to `/running_page` when running on github pages
+  pathPrefix: `/running`, // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Running Page',
     siteUrl: 'https://winf42.github.io/running',
